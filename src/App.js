d4857fb837b765c38a845dcfcde610c8651f1d1a
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-03 19:46:27 +0300

    install app and create components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer';
+import CommentSection from './components/CommentSection/CommentSection';
+
+function App() {
+	return (
+		<div className="App">
+			<SearchBar />
+			<PostContainer />
+			<CommentSection />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/posts.js
@@ -0,0 +1,99 @@
+const posts = [
+	{
+		id: 'A',
+		username: 'scarface',
+		userimg: 'https://cdn140.picsart.com/266948527031202.jpg',
+		imageUrl: 'https://cdn141.picsart.com/294396278147201.jpg',
+		likes: 400,
+		timestamp: 'May 15th 2019, 16:02:40 pm',
+		comments: [
+			{
+				id: '1',
+				username: 'scarface',
+				text: 'Focus on you...'
+			},
+			{
+				id: '2',
+				username: 'dasigndasy',
+				text: 'So beautiful!'
+			},
+			{
+				id: '3',
+				username: 'lukatist',
+				text: 'Beautiful like you!'
+			}
+		]
+	},
+	{
+		id: 'B',
+		username: 'kawaii',
+		userimg: 'https://cdn131.picsart.com/237287389058202.jpg',
+		imageUrl: 'https://cdn141.picsart.com/294512243181201.jpg',
+		likes: 4307,
+		timestamp: 'May 3th 2019, 13:00:06 pm',
+		comments: [
+			{
+				id: '4',
+				username: 'kawaii',
+				text: 'Catch ya!'
+			},
+			{
+				id: '5',
+				username: 'frittas',
+				text: 'Walk with me!!!'
+			},
+			{
+				id: '6',
+				username: 'timtom',
+				text: 'Vintage style, cute'
+			},
+			{
+				id: '7',
+				username: 'morisaf',
+				text: 'Where is this place?'
+			}
+		]
+	},
+	{
+		id: 'C',
+		username: 'playhearthstone',
+		userimg: 'https://cdn65.picsart.com/190599638002202.jpg',
+		imageUrl: 'https://cdn141.picsart.com/294541211244201.jpg',
+		likes: 5306,
+		timestamp: 'April 1th 2019, 08:26:10 am',
+		comments: [
+			{
+				id: '9',
+				username: 'playhearthstone',
+				text: 'I will buy this car!'
+			},
+			{
+				id: '10',
+				username: 'vina',
+				text: 'When you do, take me to a ride.'
+			},
+			{
+				id: '11',
+				username: 'crista',
+				text: 'Uuu pretty cool!'
+			},
+			{
+				id: '12',
+				username: 'carstravel',
+				text: 'visit us'
+			},
+			{
+				id: '13',
+				username: 'martin',
+				text: 'You have a better one!'
+			},
+			{
+				id: '8',
+				username: 'lemmy',
+				text: 'more pics!'
+			}
+		]
+	}
+];
+
+export default posts;
