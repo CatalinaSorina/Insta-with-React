a07a778ef82d6ca1ff28b54b9b1a5df0ad2e28e8
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-03 20:02:39 +0300

    PostContainer.js component

---
+++ src/components/PostContainer/PostContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Post from './Post';
+import posts from '../posts';
+import './PostContainer.css';
+
+const PostContainer = () => {
+	return (
+		<div>
+			{posts.map((post) => (
+				<Post
+					userImg={post.userimg}
+					username={post.username}
+					img={post.imageUrl}
+					likes={post.likes}
+					commentsNumber={post.comments.length}
+					comments={post.comments}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default PostContainer;
