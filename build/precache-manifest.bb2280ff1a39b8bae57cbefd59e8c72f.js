038ce60cd92525c03be97468ecfc78ac79531fbe
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-04 20:30:19 +0300

    creating deploy and adding images

---
+++ build/precache-manifest.bb2280ff1a39b8bae57cbefd59e8c72f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a313bbc1c69b401243e9196c6d167098",
+    "url": "/Insta-with-React/index.html"
+  },
+  {
+    "revision": "3952863b8e67548649c2",
+    "url": "/Insta-with-React/static/css/main.7c89110e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Insta-with-React/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3952863b8e67548649c2",
+    "url": "/Insta-with-React/static/js/main.d8cfa47f.chunk.js"
+  },
+  {
+    "revision": "3055ec310a6e67597475",
+    "url": "/Insta-with-React/static/js/runtime~main.df435ebd.js"
+  }
+]);
