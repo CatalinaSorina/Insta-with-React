7a9877413633ebb446e7b4770b6d1093902d0af3
CatalinaSorina <gherzan.catalina@yahoo.com>
2019-05-22 23:33:27 +0300

    Updates

--- precache-manifest.19859eaf49b80850f00e126893c8c89a.js
+++ precache-manifest.19859eaf49b80850f00e126893c8c89a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "efd3d9967deff2f497cdbdccf6627448",
+    "revision": "9c1facd5b27852a01443b0dbf3c4ee09",
     "url": "/Insta-with-React/index.html"
   },
   {
-    "revision": "43d07a170f211300d96c",
-    "url": "/Insta-with-React/static/css/main.36453583.chunk.css"
+    "revision": "c117ac3423de470b43f4",
+    "url": "/Insta-with-React/static/css/main.b7fc492a.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/Insta-with-React/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "43d07a170f211300d96c",
-    "url": "/Insta-with-React/static/js/main.8c7426d1.chunk.js"
+    "revision": "c117ac3423de470b43f4",
+    "url": "/Insta-with-React/static/js/main.129c3264.chunk.js"
   },
   {
     "revision": "3055ec310a6e67597475",
